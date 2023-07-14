157206d43535457e1cb6238d905d0deaeab6724d
howaboutdale <1daletromp@gmail.com>
2023-07-14 10:59:26 +0930

    Tweaked wording

--- src/components/Projects/Project3.js
+++ src/components/Projects/Project3.js
@@ -6,7 +6,7 @@ const Project3 = () => {
         <div className="container">
             <h1>DND Character Sheet App</h1>
             <p className="project-description">
-                This was my first major project using Python and Flask. I intended to create an app that would allow a person to go through the process of creating a Dungeons & Dragons character sheet. The product is defintely rough around the edges but given the time constraints that were in place and how new I was to the technologies used, I am happy with what I achieved.
+                This was my first major project using Python and Flask. It's an app that allows a person to go through the process of creating a Dungeons & Dragons character sheet and store it online. The product is rough around the edges but given the time constraints that were in place and how new I was to the technologies used, I am happy with what I achieved.
             </p>
             <p>Tech Used: Python, Flask</p>
             <Button component="a" href="https://github.com/howaboutdale/DnD_app" target='_blank'>GitHub Repo</Button>
