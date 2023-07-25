4a244368b8e3efc15ff4f6758bb2c461ac858e2a
howaboutdale <1daletromp@gmail.com>
2023-07-25 12:13:53 +0930

    Slight Tweaks

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,14 +1,22 @@
 import { Button } from "@mui/material"
 import { Stack } from '@mui/material'
+
 const AboutMe = ({ handleProjectsClick }) => {
     return (
         <div className="container">
             <h1>About Me</h1>
             <section className="about-me-blurb">
-                <p className='subtext'>Hello, I'm Dale, a Software Engineer based in Adelaide.</p>
-                <p> I possess a strong foundation in software development principles and have a keen interest in honing my skills in various programming languages and technologies.</p>
-                <p>With a meticulous approach to coding, I strive to create clean and efficient solutions while ensuring adherence to industry best practices. I enjoy the process of troubleshooting and debugging, finding satisfaction in identifying and resolving issues to enhance the overall quality of software.</p>
-                <p className='subtext'>If you require a dedicated and enthusiastic team member to contribute to your projects, I'm ready to collaborate and expand my expertise with you.</p>
+                <p className='subtext'>
+                    Hello, I'm Dale.
+                    <br />
+                    A Software Engineer based in Adelaide.
+                    <br />
+                    <br />
+                    I possess a strong foundation in software development principles and have a keen interest in honing my skills in various programming languages and technologies.
+                    With a meticulous approach to coding, I strive to create clean and efficient solutions while ensuring adherence to industry best practices. I enjoy the process of troubleshooting and debugging, finding satisfaction in identifying and resolving issues to enhance the overall quality of software.
+                    <br />
+                    <br />
+                    If you require a dedicated and enthusiastic team member to contribute to your projects, I'm ready to collaborate and expand my expertise with you.</p>
             </section>
             <Stack>
                 <Button onClick={handleProjectsClick}>Projects</Button>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,6 +9,7 @@ const Landing = ({ handleAboutMeClick, handleProjectsClick }) => {
     return (
         <div className='container'>
             <p className='main-header'>Dale Tromp</p>
+            <p className='title'>Software Engineer</p>
             <p className='subtext'>Transforming Ideas into Innovative Software Solutions.</p>
             <p className='subtext'>I am currently seeking employment, if you are in need of an adaptable, driven and collaborative helping hand in your workplace, or even just for your next project. Please feel free to follow the links and reach out!</p>
             <Stack>
